4c157dc113a9e5c56a342b03232a6416dca449ec
@Jean-Franck <jeanfranck.tang@gmail.com>
2020-03-24 10:58:07 +0100

    Front Connexion
    Affichage et début de fonction de vérification

---
+++ ESC2020/ESC2020/ClientApp/src/app/Model/Users.ts
@@ -0,0 +1,11 @@
+export class Users {
+  UserId: string;
+  Email: string;
+  Password: string;
+  Salt: string;
+  BirthDate: string;
+  Description: string;
+  FirstName: string;
+  LastName: string;
+  Job: string;
+}
