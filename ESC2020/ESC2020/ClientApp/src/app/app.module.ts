ee858b62e001b618c80548efcf05155f4c4f190c
nicolasKACEM <48383108+nicolasKACEM@users.noreply.github.com>
2020-03-27 12:45:15 +0100

    modif

--- ESC2020/ESC2020/ClientApp/src/app/app.module.ts
+++ ESC2020/ESC2020/ClientApp/src/app/app.module.ts
@@ -4,9 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/fr';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
--- ESC2020/ESC2020/Migrations/20200327112922_esc2020.Designer.cs
+++ ESC2020/ESC2020/Migrations/20200327112922_esc2020.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ESC2020.Migrations
 {
     [DbContext(typeof(ElectionContext))]
-    [Migration("20200326154640_esc2020")]
+    [Migration("20200327112922_esc2020")]
     partial class esc2020
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
