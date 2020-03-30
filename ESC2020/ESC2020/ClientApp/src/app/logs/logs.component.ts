d8e4232a14ae4faa69983ea0dffe44c3cbdd6b7f
Stevent LE MOINE <steven.le_moine@outlook.com>
2020-03-30 17:14:29 +0200

    relier logs fini

--- ESC2020/ESC2020/ClientApp/src/app/logs/logs.component.ts
+++ ESC2020/ESC2020/ClientApp/src/app/logs/logs.component.ts
@@ -1,87 +1,131 @@
 import { Component, OnInit } from '@angular/core';
 import { Log } from '../Model/Log';
 import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Router, NavigationEnd } from '@angular/router';
+import { Users } from '../Model/Users';
 
 @Component({
-  selector: 'app-logs',
-  templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.css']
+    selector: 'app-logs',
+    templateUrl: './logs.component.html',
+    styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
-  private voteList: Log[] = [];
-  private objectionList: Log[] = [];
-  private notificationList: Log[] = [];
+    private voteList: Log[] = [];
+    private objectionList: Log[] = [];
+    private notificationList: Log[] = [];
+    private allListOpinion: Log[] = [];
+    private allListNotifs: Log[] = [];
+    private previousUrl: string;
+    private currentUrl: string;
 
-  private logList: Log[] = [];
+    private logList: Log[] = [];
 
-  constructor(private location: Location) { }
+    constructor(private location: Location, private service: HttpClient, private router: Router) {
 
-  ngOnInit() {
-    this.votePush("Rémi a voté pour Nicolas", "Je trouve que Nicolas est un bon candidat d'après son expérience", "08:24");
-    this.votePush("Nicolas a voté pour Cindy", "Je trouve que Nicolas est un bon candidat d'après son expérience", "08:34");
-    this.votePush("Julien a voté pour Cindy", "Je trouve que Nicolas est un bon candidat d'après son expérience", "09:11");
-    this.votePush("Tiziano a voté pour Rémi",
-      "Je trouve que Rémi est un bon candidat, afin donc de le faire élire, j'écris un argument extrêmement long et très très très recherché car Rémi est une personne inspirante. Il m'est donc aisé d'écrire beaucoup sur cette personne, et, nous pouvons également profiter de mon message pour observer le comportement de la fenêtre de logs lorsque que quelqu'un de très très inspiré écrit un argument vraiment long. Inutilement long.",
-      "09:27");
-    this.votePush("Steven a voté pour Steven", "Je trouve que Nicolas est un bon candidat d'après son expérience", "10:11");
-    this.votePush("Cindy a voté pour Cindy", "Je trouve que Nicolas est un bon candidat d'après son expérience", "10:14");
-    this.votePush("Jean-Franck a voté pour Nicolas", "Je trouve que Nicolas est un bon candidat d'après son expérience", "10:58");
-    this.votePush("Maxime a voté pour Cindy", "Je trouve que Nicolas est un bon candidat d'après son expérience", "11:45");
+    }
 
-    this.objectionPush("Rémi a émit une objection contre Cindy", "Non.", "08:24");
-    this.objectionPush("Cindy a émit une objection contre Nicolas", "Pas assez intelligent", "08:34");
-    this.objectionPush("Nicolas a émit une objection contre Cindy", "Si", "09:11");
+    ngOnInit() {
+        this.getNotifications();
+        this.getOpinions();
+    }
 
-    this.notificationPush("La session d'éléction est passée à la phase d'objection.", "", "07:24");
-    this.notificationPush("La session d'éléction est passée à la phase de revote.", "", "08:24");
-    this.notificationPush("Jean-Franck Tang a refusé le poste de Représentant du délégué.", "", "07:24");
+    ShowVotes() {
+        this.logList = this.voteList;
+        document.getElementById("VotesTab").style.cssText = "border-bottom: 5px solid #430640;";
+        document.getElementById("ObjectionsTab").style.cssText = "border-bottom: 0px solid #430640;";
+        document.getElementById("NotificationsTab").style.cssText = "border-bottom: 0px solid #430640;";
+    }
+    ShowObjections() {
+        this.logList = this.objectionList;
+        document.getElementById("ObjectionsTab").style.cssText = "border-bottom: 5px solid #430640;";
+        document.getElementById("VotesTab").style.cssText = "border-bottom: 0px solid #430640;";
+        document.getElementById("NotificationsTab").style.cssText = "border-bottom: 0px solid #430640;";
+    }
+    ShowNotifications() {
+        this.logList = this.notificationList;
+        document.getElementById("NotificationsTab").style.cssText = "border-bottom: 5px solid #430640;";
+        document.getElementById("ObjectionsTab").style.cssText = "border-bottom: 0px solid #430640;";
+        document.getElementById("VotesTab").style.cssText = "border-bottom: 0px solid #430640;";
+    }
 
-  }
+    votePush(title: string, opinion: string, hour: string) {
+        let customLog = new Log();
+        customLog.title = title;
+        customLog.opinion = opinion;
+        customLog.hour = hour;
+        this.voteList.push(customLog);
+    }
 
-  ShowVotes() {
-    this.logList = this.voteList;
-      document.getElementById("VotesTab").style.cssText = "border-bottom: 5px solid #430640;";
-      document.getElementById("ObjectionsTab").style.cssText = "border-bottom: 0px solid #430640;";
-      document.getElementById("NotificationsTab").style.cssText = "border-bottom: 0px solid #430640;";
-  }
-  ShowObjections() {
-    this.logList = this.objectionList;
-      document.getElementById("ObjectionsTab").style.cssText = "border-bottom: 5px solid #430640;";
-      document.getElementById("VotesTab").style.cssText = "border-bottom: 0px solid #430640;";
-      document.getElementById("NotificationsTab").style.cssText = "border-bottom: 0px solid #430640;";
-  }
-  ShowNotifications() {
-    this.logList = this.notificationList;
-      document.getElementById("NotificationsTab").style.cssText = "border-bottom: 5px solid #430640;";
-      document.getElementById("ObjectionsTab").style.cssText = "border-bottom: 0px solid #430640;";
-      document.getElementById("VotesTab").style.cssText = "border-bottom: 0px solid #430640;";
-  }
+    objectionPush(title: string, opinion: string, hour: string) {
+        let customLog = new Log();
+        customLog.title = title;
+        customLog.opinion = opinion;
+        customLog.hour = hour;
+        this.objectionList.push(customLog);
+    }
 
-  votePush(title: string, opinion: string, hour: string) {
-    let customLog = new Log();
-    customLog.title = title;
-    customLog.opinion = opinion;
-    customLog.hour = hour;
-    this.voteList.push(customLog);
-  }
+    notificationPush(title: string, opinion: string, hour: string) {
+        let customLog = new Log();
+        customLog.title = title;
+        customLog.opinion = opinion;
+        customLog.hour = hour;
+        this.notificationList.push(customLog);
+    }
 
-  objectionPush(title: string, opinion: string, hour: string) {
-    let customLog = new Log();
-    customLog.title = title;
-    customLog.opinion = opinion;
-    customLog.hour = hour;
-    this.objectionList.push(customLog);
-  }
+    browserReturn() {
+        this.location.back();
+    }
 
-  notificationPush(title: string, opinion: string, hour: string) {
-    let customLog = new Log();
-    customLog.title = title;
-    customLog.opinion = opinion;
-    customLog.hour = hour;
-    this.notificationList.push(customLog);
-  }
+    getNotifications() {
+        this.service.get(window.location.origin + "/api/Notifications/FromElection/" + this.router.url.split('/')[2]).subscribe(result => {
+            this.allListNotifs = result as Log[];
+            console.log(this.allListNotifs);
+            for (let i in this.allListNotifs) {
+                let datePubli: string = this.allListNotifs[i]['dateNotification'];
+                this.notificationPush(this.allListNotifs[i]['message'], "",
+                    new Date(datePubli).toLocaleDateString() + " " + new Date(datePubli).toLocaleTimeString().substring(0, 5));
+            }
+        }, error => console.error(error));
+    }
 
-  browserReturn() {
-    this.location.back();
-  }
+    getOpinions() {
+        this.service.get(window.location.origin + "/api/Opinions/election/" + this.router.url.split('/')[2]).subscribe(result => {
+            this.allListOpinion = result as Log[];
+            for (let i in this.allListOpinion) {
+                if (this.allListOpinion[i]['typeId'] == 1) {
+                    //vote
+                    let voting: Users;
+                    let voted: Users;
+                    this.service.get(window.location.origin + "/api/Users/" + this.allListOpinion[i]['authorId']).subscribe(result => {
+                        voting = result as Users;
+                        this.service.get(window.location.origin + "/api/Users/" + this.allListOpinion[i]['concernedId']).subscribe(result => {
+                            voted = result as Users;
+                            let datePubli: string = this.allListOpinion[i]['dateOpinion'];
+                            this.votePush(voting["firstName"] + " " + voting["lastName"] + " a voté pour " + voted["firstName"] + " " + voted["lastName"],
+                                this.allListOpinion[i]['reason'], new Date(datePubli).toLocaleDateString() + " " + new Date(datePubli).toLocaleTimeString().substring(0, 5));
+                        }, error => console.error(error));
+                    }, error => console.error(error));
+
+
+                }
+                else if (this.allListOpinion[i]['typeId'] == 2) {
+                    //objection
+                    let objecting: Users;
+                    let objected: Users;
+                    this.service.get(window.location.origin + "/api/Users/" + this.allListOpinion[i]['authorId']).subscribe(result => {
+                        objecting = result as Users;
+                        this.service.get(window.location.origin + "/api/Users/" + this.allListOpinion[i]['concernedId']).subscribe(result => {
+                            objected = result as Users;
+                            let datePubli: string = this.allListOpinion[i]['dateOpinion'];
+                            this.objectionPush(objecting["firstName"] + " " + objecting["lastName"] + " a émis une objection contre " + objected["firstName"] + " " + objected["lastName"],
+                                this.allListOpinion[i]['reason'], new Date(datePubli).toLocaleDateString() + " " + new Date(datePubli).toLocaleTimeString().substring(0, 5));
+                        }, error => console.error(error));
+                    }, error => console.error(error));
+                }
+
+            }
+
+        }, error => console.error(error));
+    }
 }
