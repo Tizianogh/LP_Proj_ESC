b7c2062d8bb0c84e146500c9d6120052f92d3aa2
cindy mendes <cissi_girl@hotmail.fr>
2020-03-31 14:54:59 +0200

    Vote pris en compte dans BDD et ajout de l'age

--- ESC2020/ESC2020/ClientApp/src/app/Page-election/page-election.component.ts
+++ ESC2020/ESC2020/ClientApp/src/app/Page-election/page-election.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Participant } from '../Model/Participant';
 import { Session } from '../Model/Session';
+import { TypeOpinion } from '../Model/TypeOpinion';
 import { Users } from '../Model/Users';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
@@ -18,6 +19,8 @@ export class PageElectionComponent implements OnInit {
     private connected: boolean;
     private connectedAccount: Users = new Users();
     private electionId: string;
+    private type: TypeOpinion = new TypeOpinion();
+
     currentUser: Users = new Users();
     session: Session = new Session();
     //currentParticipant: Participant;
@@ -27,7 +30,7 @@ export class PageElectionComponent implements OnInit {
     private listeUsers: Users[] = [];
     private liste: Users[] = [];
 
-    age: string;
+    age: number;
 
     constructor(private service: HttpClient, private router: Router, private authentificationService: AuthentificationService) { }
 
@@ -54,12 +57,19 @@ export class PageElectionComponent implements OnInit {
         }, error => console.error(error));
     }
 
-    actualParticipant(user: Users) {
+    actualParticipant(user: Users, birthDate: string) {
         document.getElementById("selectParticipant").style.visibility = "visible";
-        this.age = user.BirthDate;
+        
+        const currentDate: Date = new Date();
+        currentDate.getTime();
+        console.log(currentDate.toString());
+        const date: number = new Date(birthDate).getTime();
+        console.log(birthDate);
+        this.age = Math.floor(new Date(currentDate - date) / 31536000000); // 31536000000 = 1000*60*60*24*365
+        console.log(this.age);
         this.currentUser = user;
     }
-
+    
     changeColor(userId: number) {
         if (userId != this.actualClickedId) {
             document.getElementById(this.actualClickedId.toString()).style.borderColor = "black";
@@ -75,38 +85,20 @@ export class PageElectionComponent implements OnInit {
     }
 
     Vote() {
-        //console.log(this.voteForm.get('arguments').value);
-        let type;
-        this.service.get(window.location.origin + "/api/TypeOpinion/1").subscribe(result => {
-            type = result as TypeOpinion;
+        this.service.get(window.location.origin + "/api/TypeOpinions/1").subscribe(result => {
+            this.type = result as TypeOpinion;
+            this.service.post(window.location.origin + "/api/Opinions", {
+                'AuthorId': this.connectedAccount["userId"],
+                'ConcernedId': this.currentUser["userId"],
+                'Reason': document.getElementById("argumentaires").value,
+                //this.voteForm.get('arguments').value),
+                'TypeId': this.type["typeId"],
+                'Date': Date.now(),
+                'ElectionId': this.session['electionId']
+            }).subscribe(result => {
+                console.log(result);
+            }, error => console.log(error));
+
         }, error => console.error(error));
-
-        this.service.post(window.location.origin + "/api/Opinions", {
-            'AuthorId': this.connectedAccount["userId"],
-            'ConcernedId': this.currentUser["userId"],
-            'Reason': document.getElementById("argumentaires").value,
-            //this.voteForm.get('arguments').value),
-            'TypeId': type.TypeId,
-            'Date': Date.now(),
-            'ElectionId': this.session['electionId']
-        }).subscribe(result => {
-            console.log(result);
-        }, error => console.log(error));
-
-        /*
-        this.service.post(window.location.origin + "/api/Opinions/").subscribe(result => {
-            this.session = result as Session;
-
-            //récupérer la liste des participants en fonction de l'id d'une élection
-            this.service.get(window.location.origin + "/api/Participants/election/" + this.session['electionId']).subscribe(participantResult => {
-                let listeParticipants = participantResult as Participant[];
-                for (let participant in listeParticipants) {
-                    this.service.get(window.location.origin + "/api/Users/" + listeParticipants[participant]['userId']).subscribe(userResult => {
-                        this.listeUsers.push(userResult as Users);
-                    }, error => console.error(error));
-                }
-            }, error => console.error(error));
-        }, error => console.error(error));
-        */
     }
 }
