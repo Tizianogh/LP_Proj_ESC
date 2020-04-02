64a84610b6a0763cefb5672164c9272cb8773fbe
nicolasKACEM <48383108+nicolasKACEM@users.noreply.github.com>
2020-04-02 11:15:08 +0200

    commit modif

--- ESC2020/ESC2020/ClientApp/src/app/objections/objections.component.ts
+++ ESC2020/ESC2020/ClientApp/src/app/objections/objections.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Participant } from '../Model/Participant';
 import { Session } from '../Model/Session';
@@ -12,6 +12,7 @@ import { DatePipe } from '@angular/common';
 import { FunctionCall } from '@angular/compiler';
 import { FormsModule } from '@angular/forms';
 
+
 @Component({
     selector: 'app-election',
     templateUrl: './objections.component.html',
@@ -20,79 +21,102 @@ import { FormsModule } from '@angular/forms';
 
 export class ObjectionsComponent implements OnInit {
 
-    private electionId: string;
+    connected: boolean;
+    connectedAccount: Users = new Users();
+
     session: Session = new Session();
-    private connected: boolean;
-    private connectedAccount: Users = new Users();
     actualProposed: Users = new Users();
     connectedParticipant: Participant = new Participant();
-    private type: TypeOpinion = new TypeOpinion();
+    type: TypeOpinion = new TypeOpinion();
+
     host: boolean = false;
+
 
     participantsList: Participant[] = [];
     opinionsList: Opinion[] = [];
     usersList: Users[] = [];
     objectionsList: Opinion[] = [];
-
     propositions: Proposition[] = [];
 
-    constructor(private service: HttpClient, private router: Router, private authentificationService: AuthentificationService) { }
+    constructor(private service: HttpClient, private router: Router, private authentificationService: AuthentificationService) {
+        
+    }
 
     ngOnInit() {
         this.authentificationService.getConnectedFeed().subscribe(aBoolean => this.connected = aBoolean);
         this.authentificationService.getConnectedAccountFeed().subscribe(anUser => this.connectedAccount = anUser);
 
-        setInterval(() => this.getObjections(), 5000);
+        setInterval(() => this.getObjections(), 5000); // solution temporaire avant SignalR
 
+        this.mainRequest();
+    }
+
+    mainRequest() {
         //Récupérer l'id de l'élection actuelle à partir de l'url
         let regexp: RegExp = /\d/;
-        this.electionId = regexp.exec(this.router.url)[0];
-        this.service.get(window.location.origin + "/api/Elections/" + this.electionId).subscribe(result => {
+        let electionId = regexp.exec(this.router.url)[0];
+        this.service.get(window.location.origin + "/api/Elections/" + electionId).subscribe(result => {
             this.session = result as Session;
             console.log(this.session)
-            this.service.get(window.location.origin + "/api/Participants/" + this.connectedAccount['userId'] + "/" + this.session['electionId']).subscribe(result => {
-                this.connectedParticipant = result as Participant;
-                console.log(this.connectedParticipant);
-            }, error => console.log(error));
-            if (this.connectedAccount["userId"] == this.session['hostId']) {
-                this.host = true;
-            }
-            else {
-                this.host = false;
-            }
-            //récupérer la liste des participants en fonction de l'id d'une élection
-            this.service.get(window.location.origin + "/api/Participants/election/" + this.session['electionId']).subscribe(participantResult => {
-                this.participantsList = participantResult as Participant[];
-                this.service.get(window.location.origin + "/api/Users/election/" + this.session['electionId']).subscribe(userResult => {
+            this.getConnectedParticipant();
+            this.checkHost();
+            this.preStart();
+        }, error => console.error(error));
+    }
 
-                    this.usersList = userResult as Users[];
-                    //Recuperer toutes les opinions de cette election et comptabilisation des votes
-                    this.service.get(window.location.origin + "/api/Opinions/election/" + this.electionId).subscribe(result => {
-                        this.opinionsList = result as Opinion[];
-                        for (let i in this.usersList) {
-                            if (this.getParticipant(this.usersList[i]['userId'])['proposable']) {
-                                this.propositions.push(new Proposition(this.usersList[i]['userId'], 0));
-                                for (let j in this.opinionsList) {
-                                    if (this.opinionsList[j]['typeId'] == 1) {
-                                        if (this.opinionsList[j]['concernedId'] == this.usersList[i]['userId']) {
-                                            this.propositions[i].VoteCounter++;
-                                        }
-                                    }
+    getConnectedParticipant() {
+        this.service.get(window.location.origin + "/api/Participants/" + this.connectedAccount['userId'] + "/" + this.session['electionId']).subscribe(result => {
+            this.connectedParticipant = result as Participant;
+            console.log(this.connectedParticipant);
+        }, error => console.log(error));
+    }
+
+    checkHost() {
+        if (this.connectedAccount["userId"] == this.session['hostId']) {
+            this.host = true;
+        }
+        else {
+            this.host = false;
+        }
+    }
+
+    preStart() {
+        //récupérer la liste des participants en fonction de l'id d'une élection
+        this.service.get(window.location.origin + "/api/Participants/election/" + this.session['electionId']).subscribe(participantResult => {
+            this.participantsList = participantResult as Participant[];
+            this.startCounting();
+        }, error => console.error(error));
+    }
+
+    startCounting() {
+        this.service.get(window.location.origin + "/api/Users/election/" + this.session['electionId']).subscribe(userResult => {
+
+            this.usersList = userResult as Users[];
+            //Recuperer toutes les opinions de cette election et comptabilisation des votes
+            this.service.get(window.location.origin + "/api/Opinions/election/" + this.session['electionId']).subscribe(result => {
+                this.opinionsList = result as Opinion[];
+                for (let i in this.usersList) {
+                    if (this.getParticipant(this.usersList[i]['userId'])['proposable']) {
+                        this.propositions.push(new Proposition(this.usersList[i]['userId'], 0));
+                        for (let j in this.opinionsList) {
+                            if (this.opinionsList[j]['typeId'] == 1) {
+                                if (this.opinionsList[j]['concernedId'] == this.usersList[i]['userId']) {
+                                    this.propositions[i].VoteCounter++;
                                 }
                             }
                         }
+                    }
+                }
 
-                        //deroulement
-                        this.sortPropositions();
-                        console.log(this.propositions);
-                        for (let i in this.usersList) {
-                            if (this.usersList[i]['userId'] == this.propositions[0].UserId) {
-                                this.actualProposed = this.usersList[i];
-                            }
-                        }
-                    });
-                }, error => console.error(error));
-            }, error => console.error(error));
+                //deroulement
+                this.sortPropositions();
+                console.log(this.propositions);
+                for (let i in this.usersList) {
+                    if (this.usersList[i]['userId'] == this.propositions[0].UserId) {
+                        this.actualProposed = this.usersList[i];
+                    }
+                }
+            });
         }, error => console.error(error));
     }
 
@@ -106,7 +130,7 @@ export class ObjectionsComponent implements OnInit {
 
     sortPropositions() {
         let tmp: Proposition[] = [];
-        let copy = this.propositions.copyWithin(0, this.propositions.length);
+        let copy = (<any>this.propositions).copyWithin(0, this.propositions.length);
         while (copy.length > 0) {
             let cpt = 0
             for (let i = 0; i < copy.length; i++) {
@@ -131,7 +155,7 @@ export class ObjectionsComponent implements OnInit {
                 'Date': Date.now(),
                 'ElectionId': this.session['electionId']
             }).subscribe(result => {
-                document.getElementById("argumentaires").value = "";
+                (<HTMLInputElement>document.getElementById("argumentaires")).value = "";
             }, error => console.log(error));
         }, error => console.error(error));
     }
@@ -156,7 +180,7 @@ export class ObjectionsComponent implements OnInit {
                     this.objectionsList.push(tempObjectionsList[i])
                 }
             }
-           
+
         }, error => console.log(error));
     }
 
@@ -182,35 +206,37 @@ export class ObjectionsComponent implements OnInit {
                     this.objectionsList.push(tempObjectionsList[i])
                 }
             }
-           
 
-            this.service.get(window.location.origin + "/api/Participants/election/" + this.session['electionId']).subscribe(participantResult => {
-                this.participantsList = participantResult as Participant[];
-                for (let i = 0; i < this.participantsList.length; i++) {
-                    this.service.put(window.location.origin + "/api/Participants/" + this.participantsList[i]['userId'] + "/" + this.session['electionId'], {
-                        "UserId": this.participantsList[i]['userId'],
-                        "ElectionId": this.session['electionId'],
-                        "HasTalked": false,
-                        "Proposable": this.participantsList[i]["proposable"]
-                    }).subscribe(result => {
+            this.updateParticipantForVote()
 
-                    }, error => console.log(error));
-                }
-            }, error => console.error(error));
-
-            
 
         }, error => console.log(error));
-        
+
     }
 
     ngOnDestroy() {
+        
+    }
 
+    updateParticipantForVote() {
+
+        this.service.get(window.location.origin + "/api/Participants/election/" + this.session['electionId']).subscribe(participantResult => {
+            this.participantsList = participantResult as Participant[];
+            for (let i = 0; i < this.participantsList.length; i++) {
+                this.service.put(window.location.origin + "/api/Participants/" + this.participantsList[i]['userId'] + "/" + this.session['electionId'], {
+                    "UserId": this.participantsList[i]['userId'],
+                    "ElectionId": this.session['electionId'],
+                    "HasTalked": false,
+                    "Proposable": this.participantsList[i]["proposable"]
+                }).subscribe(result => {
+
+                }, error => console.log(error));
+            }
+        }, error => console.error(error));
     }
 }
 
 class Proposition {
-
     constructor(
         public UserId: number,
         public VoteCounter: number
