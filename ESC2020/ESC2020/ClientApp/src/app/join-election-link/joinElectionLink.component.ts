3dcfd874446192614be4dc9859ed313250e59275
nicolasKACEM <48383108+nicolasKACEM@users.noreply.github.com>
2020-04-03 11:27:43 +0200

    fix bug

--- ESC2020/ESC2020/ClientApp/src/app/join-election-link/joinElectionLink.component.ts
+++ ESC2020/ESC2020/ClientApp/src/app/join-election-link/joinElectionLink.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
-import { Users, AuthentificationService } from '../services/authentification.service';
+import { AuthentificationService } from '../services/authentification.service';
 import { Participant } from '../Model/Participant';
 import { Session } from '../Model/Session';
+import { Users } from '../Model/Users';
 @Component({
     selector: 'app-creation',
     templateUrl: './joinElectionLink.component.html',
