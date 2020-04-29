005794344bfe7fe754e71bd3b79cf05799dde7d6
Clement868 <c.bisson.jouvet@gmail.com>
2020-04-29 12:38:45 +0200

    bugs fix

--- ESC2020/ESC2020/Migrations/ElectionContextModelSnapshot.cs
+++ ESC2020/ESC2020/Migrations/ElectionContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace ESC2020.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
-                .HasAnnotation("ProductVersion", "3.1.3")
+                .HasAnnotation("ProductVersion", "3.1.2")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             modelBuilder.Entity("ESC2020.Model.Election", b =>
